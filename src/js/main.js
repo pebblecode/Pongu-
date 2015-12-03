3bc9abcd281298b7a7365216ece56a7603c4feb5
Peter Tait <contact@petertait.com>
2015-12-03 11:47:18 +0000

    add: font, game over on all walls, one paddle

--- src/js/main.js
+++ src/js/main.js
@@ -1,171 +1,396 @@
-var animate = window.requestAnimationFrame || window.webkitRequestAnimationFrame || window.mozRequestAnimationFrame || function (callback) {
-  window.setTimeout(callback, 1000 / 60);
+// RequestAnimFrame: a browser API for getting smooth animations
+window.requestAnimFrame = (function(){
+  return  window.requestAnimationFrame       || 
+    window.webkitRequestAnimationFrame || 
+    window.mozRequestAnimationFrame    || 
+    window.oRequestAnimationFrame      || 
+    window.msRequestAnimationFrame     ||  
+    function( callback ){
+      return window.setTimeout(callback, 1000 / 60);
+    };
+})();
+
+window.cancelRequestAnimFrame = ( function() {
+  return window.cancelAnimationFrame          ||
+    window.webkitCancelRequestAnimationFrame    ||
+    window.mozCancelRequestAnimationFrame       ||
+    window.oCancelRequestAnimationFrame     ||
+    window.msCancelRequestAnimationFrame        ||
+    clearTimeout
+} )();
+
+
+// Initialize canvas and required variables
+var canvas = document.getElementById("canvas"),
+    ctx = canvas.getContext("2d"), // Create canvas context
+    W = window.innerWidth, // Window's width
+    H = window.innerHeight, // Window's height
+    particles = [], // Array containing particles
+    ball = {}, // Ball object
+    paddles = [2], // Array containing two paddles
+    mouse = {}, // Mouse object to store it's current position
+    points = 0, // Varialbe to store points
+    fps = 60, // Max FPS (frames per second)
+    particlesCount = 20, // Number of sparks when ball strikes the paddle
+    flag = 0, // Flag variable which is changed on collision
+    particlePos = {}, // Object to contain the position of collision 
+    multipler = 1, // Varialbe to control the direction of sparks
+    startBtn = {}, // Start button object
+    restartBtn = {}, // Restart button object
+    over = 0, // flag varialbe, cahnged when the game is over
+    init, // variable to initialize animation
+    paddleHit;
+
+// Add mousemove and mousedown events to the canvas
+canvas.addEventListener("mousemove", trackPosition, true);
+canvas.addEventListener("mousedown", btnClick, true);
+
+// Initialise the collision sound
+collision = document.getElementById("collide");
+
+// Set the canvas's height and width to full screen
+canvas.width = W;
+canvas.height = H;
+
+// Function to paint canvas
+function paintCanvas() {
+  ctx.fillStyle = "black";
+  ctx.fillRect(0, 0, W, H);
+}
+
+// Function for creating paddles
+function Paddle(pos) {
+  // Height and width
+  this.h = 30;
+  this.w = 150;
+  
+  // Paddle's position
+  this.x = W/2 - this.w/2;
+  this.y = (pos == "top") ? 0 : H - this.h;
+}
+
+// Push two new paddles into the paddles[] array
+paddles.push(new Paddle("bottom"));
+paddles.push(new Paddle("top"));
+
+// Ball object
+ball = {
+  x: 160,
+  y: 160, 
+  r: 30,
+  c: "white",
+  vx: 4,
+  vy: 8,
+  
+  // Function for drawing ball on canvas
+  draw: function() {
+    ctx.beginPath();
+    ctx.fillStyle = this.c;
+    ctx.fillRect(this.x, this.y, this.r, this.r);
+    ctx.fill();
+  }
 };
 
-var canvas = document.getElementById("pongu");
-var width = 400;
-var height = 500;
-canvas.width = width;
-canvas.height = height;
-var context = canvas.getContext('2d');
-var player = new Player();
-var computer = new Computer();
-var ball = new Ball(200, 300);
-
-var keysDown = {};
-
-var render = function () {
-  context.fillStyle = "#000000";
-  context.fillRect(0, 0, width, height);
-  player.render();
-  // computer.render();
-  ball.render();
+// Start Button object
+startBtn = {
+  w: 300,
+  h: 100,
+  x: W/2 - 150,
+  y: H/2 - 56,
+  
+  draw: function() {
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = "10";
+    ctx.strokeRect(this.x, this.y, this.w, this.h);
+    
+    ctx.font = "56px minecraft, sans-serif";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillStlye = "white";
+    ctx.fillText("START", W/2, H/2 );
+  }
 };
 
-var update = function () {
-  player.update();
-  // computer.update(ball);
-  ball.update(player.paddle, computer.paddle);
+// Restart Button object
+restartBtn = {
+  w: 360,
+  h: 100,
+  x: W/2 - 180,
+  y: H/2 - 82,
+  
+  draw: function() {
+    ctx.strokeStyle = "white";
+    ctx.lineWidth = "10";
+    ctx.strokeRect(this.x, this.y, this.w, this.h);
+    
+    ctx.font = "56px minecraft, sans-serif";
+    ctx.textAlign = "center";
+    ctx.textBaseline = "middle";
+    ctx.fillStlye = "white";
+    ctx.fillText("RESTART", W/2, H/2 - 25 );
+  }
 };
 
-var step = function () {
+// Function for creating particles object
+function createParticles(x, y, m) {
+  this.x = x || 0;
+  this.y = y || 0;
+  
+  this.radius = 3;
+  
+  this.vx = -1.5 + Math.random()*3;
+  this.vy = m * Math.random()*1.5;
+}
+
+// Draw everything on canvas
+function draw() {
+  paintCanvas();
+  for(var i = 0; i < paddles.length; i++) {
+    p = paddles[1];
+    
+    ctx.fillStyle = "white";
+    ctx.fillRect(p.x, p.y, p.w, p.h);
+  }
+  
+  ball.draw();
   update();
-  render();
-  animate(step);
-};
-
-function Paddle(x, y, width, height) {
-  this.x = x;
-  this.y = y;
-  this.width = width;
-  this.height = height;
-  this.x_speed = 0;
-  this.y_speed = 0;
 }
 
-Paddle.prototype.render = function () {
-  context.fillStyle = "#ffffff";
-  context.fillRect(this.x, this.y, this.width, this.height);
-};
-
-Paddle.prototype.move = function (x, y) {
-  this.x += x;
-  this.y += y;
-  this.x_speed = x;
-  this.y_speed = y;
-  if (this.x < 0) {
-    this.x = 0;
-    this.x_speed = 0;
-  } else if (this.x + this.width > 400) {
-    this.x = 400 - this.width;
-    this.x_speed = 0;
-  }
-};
-
-function Computer() {
-  this.paddle = new Paddle(175, 10, 50, 10);
-}
-
-Computer.prototype.render = function () {
-  // this.paddle.render();
-};
-
-Computer.prototype.update = function (ball) {
-  var x_pos = ball.x;
-  var diff = -((this.paddle.x + (this.paddle.width / 2)) - x_pos);
-  if (diff < 0 && diff < -4) {
-      diff = -5;
-  } else if (diff > 0 && diff > 4) {
-      diff = 5;
-  }
-  this.paddle.move(diff, 0);
-  if (this.paddle.x < 0) {
-      this.paddle.x = 0;
-  } else if (this.paddle.x + this.paddle.width > 400) {
-      this.paddle.x = 400 - this.paddle.width;
-  }
-};
-
-function Player() {
-  this.paddle = new Paddle(175, 490, 50, 10);
-}
-
-Player.prototype.render = function () {
-  this.paddle.render();
-};
-
-Player.prototype.update = function () {
-  for (var key in keysDown) {
-    var value = Number(key);
-    if (value == 37) {
-        this.paddle.move(-4, 0);
-    } else if (value == 39) {
-        this.paddle.move(4, 0);
-    } else {
-        this.paddle.move(0, 0);
+// Function to increase speed after every 5 points
+function increaseSpd() {
+  if(points % 4 === 0) {
+    if(Math.abs(ball.vx) < 15) {
+      ball.vx += (ball.vx < 0) ? -1 : 1;
+      ball.vy += (ball.vy < 0) ? -2 : 2;
     }
   }
-};
-
-function Ball(x, y) {
-  this.x = x;
-  this.y = y;
-  this.x_speed = 0;
-  this.y_speed = 5;
 }
 
-Ball.prototype.render = function () {
-  context.beginPath();
-  context.arc(this.x, this.y, 10, 2 * Math.PI, false);
-  context.fillStyle = "#ffffff";
-  context.fill();
-};
+// Track the position of mouse cursor
+function trackPosition(e) {
+  mouse.x = e.pageX;
+  mouse.y = e.pageY;
+}
 
-Ball.prototype.update = function (paddle1, paddle2) {
-  this.x += this.x_speed;
-  this.y += this.y_speed;
-  var top_x = this.x - 5;
-  var top_y = this.y - 5;
-  var bottom_x = this.x + 5;
-  var bottom_y = this.y + 5;
-
-  if (this.x - 5 < 0) {
-    this.x = 5;
-    this.x_speed = -this.x_speed;
-  } else if (this.x + 5 > 400) {
-    this.x = 395;
-    this.x_speed = -this.x_speed;
+// Function to update positions, score and everything.
+// Basically, the main game logic is defined here
+function update() {
+  
+  // Update scores
+  updateScore(); 
+  
+  // Move the paddles on mouse move
+  if(mouse.x && mouse.y) {
+    for(var i = 1; i < paddles.length; i++) {
+      p = paddles[i];
+      p.x = mouse.x - p.w/2;
+    }   
   }
-
-  if (this.y < 0 || this.y > 500) {
-    this.x_speed = 0;
-    this.y_speed = 3;
-    this.x = 200;
-    this.y = 300;
+  
+  // Move the ball
+  ball.x += ball.vx;
+  ball.y += ball.vy;
+  
+  // Collision with paddles
+  p1 = paddles[1];
+  p2 = paddles[2];
+  
+  // If the ball strikes with paddles,
+  // invert the y-velocity vector of ball,
+  // increment the points, play the collision sound,
+  // save collision's position so that sparks can be
+  // emitted from that position, set the flag variable,
+  // and change the multiplier
+  if(collides(ball, p1)) {
+    collideAction(ball, p1);
   }
-
-  if (top_y > 300) {
-    if (top_y < (paddle1.y + paddle1.height) && bottom_y > paddle1.y && top_x < (paddle1.x + paddle1.width) && bottom_x > paddle1.x) {
-      this.y_speed = -3;
-      this.x_speed += (paddle1.x_speed / 2);
-      this.y += this.y_speed;
+  
+  else if(collides(ball, p2)) {
+    // collideAction(ball, p2);
+  } 
+  
+  else {
+    // Collide with walls, If the ball hits the top/bottom,
+    // walls, run gameOver() function
+    if(ball.y + ball.r > H) {
+      ball.y = H - ball.r;
+      gameOver();
+    } 
+    
+    else if(ball.y < 0) {
+      ball.y = ball.r;
+      gameOver();
     }
-  } else {
-    if (top_y < (paddle2.y + paddle2.height) && bottom_y > paddle2.y && top_x < (paddle2.x + paddle2.width) && bottom_x > paddle2.x) {
-      this.y_speed = 3;
-      this.x_speed += (paddle2.x_speed / 2);
-      this.y += this.y_speed;
+    
+    // If ball strikes the vertical walls, invert the 
+    // x-velocity vector of ball
+    if(ball.x + ball.r > W) {
+      // ball.vx = -ball.vx;
+      // ball.x = W - ball.r;
+      gameOver();
+    }
+    
+    else if(ball.x -ball.r < 0) {
+      // ball.vx = -ball.vx;
+      // ball.x = ball.r;
+      // gameOver();
     }
   }
-};
+  
+  // If flag is set, push the particles
+  if(flag == 1) { 
+    for(var k = 0; k < particlesCount; k++) {
+      particles.push(new createParticles(particlePos.x, particlePos.y, multiplier));
+    }
+  } 
+  
+  // Emit particles/sparks
+  emitParticles();
+  
+  // reset flag
+  flag = 0;
+}
 
-document.body.appendChild(canvas);
-animate(step);
+//Function to check collision between ball and one of
+//the paddles
+function collides(b, p) {
+  if(b.x + ball.r >= p.x && b.x - ball.r <=p.x + p.w) {
+    if(b.y >= (p.y - p.h) && p.y > 0){
+      paddleHit = 1;
+      return true;
+    }
+    
+    else if(b.y <= p.h && p.y === 0) {
+      paddleHit = 2;
+      return true;
+    }
+    
+    else return false;
+  }
+}
 
-window.addEventListener("keydown", function (event) {
-  keysDown[event.keyCode] = true;
-});
+//Do this when collides === true
+function collideAction(ball, p) {
+  ball.vy = -ball.vy;
+  
+  if(paddleHit === 1) {
+    ball.y = p.y - p.h;
+    particlePos.y = ball.y + ball.r;
+    multiplier = -1;  
+  }
+  
+  else if(paddleHit === 2) {
+    ball.y = p.h + ball.r;
+    particlePos.y = ball.y - ball.r;
+    multiplier = 1; 
+  }
+  
+  points++;
+  increaseSpd();
+  
+  if(collision) {
+    if(points > 0) 
+      collision.pause();
+    
+    collision.currentTime = 0;
+    collision.play();
+  }
+  
+  particlePos.x = ball.x;
+  flag = 1;
+}
 
-window.addEventListener("keyup", function (event) {
-  delete keysDown[event.keyCode];
-});
+// Function for emitting particles
+function emitParticles() { 
+  for(var j = 0; j < particles.length; j++) {
+    par = particles[j];
+    
+    ctx.beginPath(); 
+    ctx.fillStyle = "white";
+    if (par.radius > 0) {
+      ctx.arc(par.x, par.y, par.radius, 0, Math.PI*2, false);
+    }
+    ctx.fill();  
+    
+    par.x += par.vx; 
+    par.y += par.vy; 
+    
+    // Reduce radius so that the particles die after a few seconds
+    par.radius = Math.max(par.radius - 0.05, 0.0); 
+    
+  } 
+}
+
+// Function for updating score
+function updateScore() {
+  ctx.fillStyle = "white";
+  ctx.font = "30px minecraft, sans-serif";
+  ctx.textAlign = "left";
+  ctx.textBaseline = "top";
+  ctx.fillText(points, 20, 20 );
+}
+
+// Function to run when the game overs
+function gameOver() {
+  ctx.fillStlye = "white";
+  ctx.font = "20px minecraft, sans-serif";
+  ctx.textAlign = "center";
+  ctx.textBaseline = "middle";
+  ctx.fillText("GAME OVER! SCORE: "+points+"", W/2, H/2 + 50 );
+  
+  // Stop the Animation
+  cancelRequestAnimFrame(init);
+  
+  // Set the over flag
+  over = 1;
+  
+  // Show the restart button
+  restartBtn.draw();
+}
+
+// Function for running the whole animation
+function animloop() {
+  init = requestAnimFrame(animloop);
+  draw();
+}
+
+// Function to execute at startup
+function startScreen() {
+  setTimeout(function () {
+    draw();
+    startBtn.draw();
+  }, 100);
+}
+
+// On button click (Restart and start)
+function btnClick(e) {
+  
+  // Variables for storing mouse position on click
+  var mx = e.pageX,
+      my = e.pageY;
+  
+  // Click start button
+  if(mx >= startBtn.x && mx <= startBtn.x + startBtn.w) {
+    animloop();
+    
+    // Delete the start button after clicking it
+    startBtn = {};
+  }
+  
+  // If the game is over, and the restart button is clicked
+  if(over == 1) {
+    if(mx >= restartBtn.x && mx <= restartBtn.x + restartBtn.w) {
+      ball.x = 20;
+      ball.y = 20;
+      points = 0;
+      ball.vx = 4;
+      ball.vy = 8;
+      animloop();
+      
+      over = 0;
+    }
+  }
+}
+
+// Show the start screen
+startScreen();
